e660cdac61344fa5311828a86f1d9807d829714c
Khang <metchetme123@gmail.com>
2025-05-27 17:32:26 +0700

    a

--- routes/User.route.js
+++ routes/User.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authUser, registerUser, forgotPassword, verifyCode, resetPassword, changePassword, updateProfile } from "../controllers/user.controller.js";
+import { authUser, registerUser, forgotPassword, verifyCode, resetPassword, changePassword, updateProfile, verifyEmail } from "../controllers/user.controller.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const UserRoute = express.Router();
@@ -8,6 +8,7 @@ UserRoute.post("/login", authUser);
 UserRoute.post("/register", registerUser);
 UserRoute.post("/forgot-password", forgotPassword);
 UserRoute.post("/verify-code", verifyCode);
+UserRoute.get("/verify-email/:token", verifyEmail);
 UserRoute.post("/reset-password", resetPassword);
 UserRoute.post("/change-password", changePassword);
 UserRoute.put("/update-profile", protect, updateProfile);
